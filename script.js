f173b8bbb385acc7432d4141037f4d61e9c8a365
christianzaboli <zabolichristian@gmail.com>
2025-09-09 20:16:39 +0200

    primo snack completato

--- script.js
+++ script.js
@@ -3,8 +3,45 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
 
+// array di oggetti
+const arrayBici = [
+
+    {
+    brand : "Trek",
+    peso : 12
+},
+    {
+    brand : "Cannondale",
+    peso : 15
+},
+    {
+    brand : "Specialized",
+    peso : 10
+},
+    {
+    brand : "Cervélo",
+    peso : 10
+},
+    {
+    brand : "Pininfarina",
+    peso : 8
+},
+
+];
+
+// ciclo l'array di biciclette
+
+let biciLeggera = arrayBici[0]; // Initialize with the first element
+
+for (let i = 0; i < arrayBici.length; i++) {
+  if (arrayBici[i].peso < biciLeggera.peso) {
+    biciLeggera = arrayBici[i];
+  }
+}
+console.log('La bicicletta piú leggera é', biciLeggera); // Output: 5
 
 
+// console.log(biciLeggera);
 
 
 
